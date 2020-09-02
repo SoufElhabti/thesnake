9d735bbc3e9bd21211c865f9040c8f1beaf279cf
mainteemo <mainteemo@mainteemo.mainteemo.boom>
2020-09-02 02:03:05 +0100

    added an awesome onliner to recon automation

--- recon.sh
+++ recon.sh
@@ -7,4 +7,7 @@ httpx -l domains  -title -web-server -status-code --follow-redirects -o infos
 
 cat domains | waybackurls > wayback
 
+# this one liner is damn good 
+#shodan domain att.com| awk '{print $3}'|  httpx -silent | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /jaeles-signatures/ -u @
+cat domains| awk '{print $3}'|  httpx -silent | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /jaeles-signatures/ -u @
 
